64a043c23e4fd7aa018e10da0dd9bb52afb9631a
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-15 09:44:52 +0100

    Configured the dispatcher servelet and view. Implemented a working view controller'

--- src/com/example/stealth104/HomeController.java
+++ src/com/example/stealth104/HomeController.java
@@ -1,4 +1,13 @@
 package com.example.stealth104;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "form";
+    }
 }

f1231407610618ee9b03cbd5973925c955d5fd67
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-15 10:10:45 +0100

    HttpServletRequest is not present in the current libs.

--- src/com/example/stealth104/HomeController.java
+++ src/com/example/stealth104/HomeController.java
@@ -1,11 +1,11 @@
 package com.example.stealth104;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
 
     @RequestMapping("/showForm")
     public String showForm() {
@@ -15,5 +15,12 @@ public class HomeController {
     @RequestMapping("/processForm")
     public String processForm() {
         return "student";
+    }
+
+    // new controller method to read form data and add
+    // data to the model
+
+    public String letsShoutDude(HttpServletRequest request, Model model ){
+        return "helloworld";
     }
 }

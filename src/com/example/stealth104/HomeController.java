8dd9100b59a1de0da07b021b310b65b106364cab
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-15 19:27:38 +0100

    Example of request params

--- src/com/example/stealth104/HomeController.java
+++ src/com/example/stealth104/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -34,6 +35,21 @@ public class HomeController {
 
         // create the message
         String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "student";
+    }
+
+    @RequestMapping("/processFormV3")
+    public String processFormv3(@RequestParam("studentName") String theName, Model model ){
+
+        // convert the data to all Cap
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey my friend from v3 ! " + theName;
 
         // add message to the model
         model.addAttribute("message", result);

d304f1d1733597670cd606023b428c6932343617
Bukunmi <oluwabukunmi.aluko@gmail.com>
2020-09-16 11:07:39 +0100

    Completed module 15

--- src/com/example/stealth104/CustomerController.java
+++ src/com/example/stealth104/CustomerController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
